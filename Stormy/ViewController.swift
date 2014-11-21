61c62ac84dedfd3f90518fde760cc00f51832e03
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-21 18:10:51 +0900

    Initial Commit

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Fumiya Nakamura on 2014-11-21.
+//  Copyright (c) 2014 Fumiya Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
