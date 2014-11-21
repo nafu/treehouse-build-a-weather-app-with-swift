d43b8dc6990ac1b53ae3e81a82aae005f2e2b0e7
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-21 20:54:05 +0900

    Add Writing Concurrent Networking Code Step

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -18,12 +18,16 @@ class ViewController: UIViewController {
 		let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
 		let forecastURL = NSURL(string: "35.658380,139.701595", relativeToURL: baseURL)
 		let weatherData = NSData(contentsOfURL: forecastURL!, options: nil, error: nil)
+		let sharedSession = NSURLSession.sharedSession()
+		let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithRequest(NSURLRequest(URL: forecastURL!), completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+			println(response)
+		})
+		downloadTask.resume()
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
 
 }
