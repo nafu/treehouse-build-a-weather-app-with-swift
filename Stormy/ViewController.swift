fe10ada923bfcded6137cb6ea8c9edbbe31b5c4e
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-21 19:17:47 +0900

    Add Networking in iOS Step

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	private let apiKey = "YOUR_API_KEY"
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+		let forecastURL = NSURL(string: "35.658380,139.701595", relativeToURL: baseURL)
 	}
 
 	override func didReceiveMemoryWarning() {
