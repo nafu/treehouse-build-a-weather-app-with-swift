69291a550990138939dd7b42347336942e256900
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-22 04:06:42 +0900

    Add Setting Icon Images Step

--- Current.swift
+++ Current.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct Current {
 	var currentTime: String?
@@ -14,7 +15,7 @@ struct Current {
 	var humidity: Double
 	var precipProbability: Double
 	var summary: String
-	var icon: String
+	var icon: UIImage?
 
 	init(weatherDictionary: NSDictionary) {
 		let currentWeather: NSDictionary = weatherDictionary["currently"] as NSDictionary
@@ -23,10 +24,12 @@ struct Current {
 		humidity = currentWeather["humidity"] as Double
 		precipProbability = currentWeather["precipProbability"] as Double
 		summary = currentWeather["summary"] as String
-		icon = currentWeather["icon"] as String
 
 		let currentTimeIntValue = currentWeather["time"] as Int
 		currentTime = dateStringFromUnixtime(currentTimeIntValue)
+
+		let iconString = currentWeather["icon"] as String
+		icon = weatherIconFromString(iconString)
 	}
 
 	func dateStringFromUnixtime(unixTime: Int) -> String {
@@ -37,5 +40,37 @@ struct Current {
 		dateFormatter.timeStyle = .ShortStyle
 
 		return dateFormatter.stringFromDate(weatherDate)
+	}
+
+	func weatherIconFromString(stringIcon: String) -> UIImage {
+		var imageName: String
+
+		switch stringIcon {
+		case "clear-day":
+			imageName = "clear-day"
+		case "clear-night":
+			imageName = "clear-night"
+		case "rain":
+			imageName = "rain"
+		case "snow":
+			imageName = "snow"
+		case "sleet":
+			imageName = "sleet"
+		case "wind":
+			imageName = "wind"
+		case "fog":
+			imageName = "fog"
+		case "cloudy":
+			imageName = "cloudy"
+		case "partly-cloudy-day":
+			imageName = "partly-cloudy"
+		case "partly-cloudy-night":
+			imageName = "cloudy-night"
+		default:
+			imageName = "default"
+		}
+
+		var iconImage = UIImage(named: imageName)
+		return iconImage!
 	}
 }
